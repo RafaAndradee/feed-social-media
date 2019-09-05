f49cf6c7dff88c186a9c413c57cfd6f67da1b836
rafandrade <rafandrade.a@gmail.com>
2019-09-04 22:52:48 -0300

    pegando dados jason

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>ESTUDO JSON</title>
+    <link rel="stylesheet" href="css/app.css">
+
+  </head>
+  <body>
+    <input type="text" id="search" class="form-control form-control-lg">
+
+    <div class="" id="match-list">
+
+    <script src="js/main.js" charset="utf-8"></script>
+    </div>
+  </body>
+</html>
