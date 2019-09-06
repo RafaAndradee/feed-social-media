6eec39bb9727f16f1ec511726e3de7e5c9016f7b
rafandrade <rafandrade.a@gmail.com>
2019-09-06 16:59:27 -0300

    criação dos dados JSON

--- js/main.js
+++ js/main.js
@@ -1,13 +1,43 @@
 const search = document.getElementById('search');
-const matchList = document.getElementById('id');
+const matchList = document.getElementById('match-list');
+let origens; //corresponde a lista de lugares disponiveis inseridos na lista .Json
 
+// PEGAR A LISTA
+const pegarOrigens = async () => {
+ const res = await fetch('./data/airport.json');
+ origens = await res.json();
+};
 
-const searchStates = async searchText => {
-  const res = await fetch('data/states.json');
-  const states = await res.json();
+// FILTRAR A LISTA
+const searchStates = searchText => {
+ // FILTRA A PALAVRA DIGITADA NA BUSCA PARA RELACIONAR COM UMA LISTA DE OBJETOS JSON
+ let matches = origens.filter(state => {
+  const regex = new RegExp(`^${searchText}`, 'gi');
+  return state.CIDADE.match(regex) || state.IATA.match(regex);
+ });
 
-  console.log(states);
-}
+ // EVITAR APARECER TUDO QUANDO CLICAR NO INPUT
+ if (searchText.length === 0) {
+  matches = [];
+  matchList.innerHTML = '';
+ }
 
+ outputHtml(matches);
+};
 
+// VER RESULTADOS
+const outputHtml = matches => {
+ if (matches.length > 0) {
+  const html = matches
+   .map(
+    match => `<div class="card card-body mb-1">
+    <h4>${match.CIDADE} (${match.IATA})
+   </div>`
+   )
+   .join('');
+  matchList.innerHTML = html;
+ }
+};
+
+window.addEventListener('DOMContentLoaded', pegarOrigens);
 search.addEventListener('input', () => searchStates(search.value));
