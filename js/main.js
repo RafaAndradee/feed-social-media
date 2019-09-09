5cc8f7250c35fc471c78be28fdeee876b1e412c0
rafandrade <rafandrade.a@gmail.com>
2019-09-09 18:33:46 -0300

    o bug no imput ainda perciste

--- js/main.js
+++ js/main.js
@@ -1,92 +1,70 @@
-const search = document.getElementById('search');
-const matchList = document.getElementById('match-list');
-let origens; //corresponde a lista de lugares disponiveis inseridos na lista .Json
+const searchOrigens = document.getElementById('searchOrigens');
+const searchDestino = document.getElementById('searchDestino');
+const matchList = document.querySelector('#destinoWrap #match-list');
+//const destList = document.querySelector('#destinoWrap #match-list');
+let locals; //corresponde a lista de lugares disponiveis inseridos na lista .Json
+
 
 // PEGAR A LISTA
-const pegarOrigens = async () => {
+const getlocals = async () => {
  const res = await fetch('./data/airport.json');
- origens = await res.json();
+ locals = await res.json();
 };
+
 
 // FILTRAR A LISTA
 const searchStates = searchText => {
  // FILTRA A PALAVRA DIGITADA NA BUSCA PARA RELACIONAR COM UMA LISTA DE OBJETOS JSON
- let matches = origens.filter(state => {
+ let matches = locals.filter(state => {
   const regex = new RegExp(`^${searchText}`, 'gi');
   return state.CIDADE.match(regex) || state.IATA.match(regex);
  });
 
- // EVITAR APARECER TUDO QUANDO CLICAR NO INPUT
- if (searchText.length === 0) {
+// EVITAR APARECER TUDO QUANDO CLICAR NO INPUT
+if (searchText.length === 0) {
   matches = [];
   matchList.innerHTML = '';
- }
-
- outputHtml(matches);
-};
-
-
-
-
-//criar uma array com os titulos de cada objeto da lista
-//criar uma função que marque o texto com uma id
-//criar um map que passei pelos objetos até chegar a li que contem a id Active
-
-
-//titleOrigens.map(function(index));
-
-function clickListener(){
-
-  const addActive = document.getElementById('listOrigin').id = 'listActive'
-  const listActive = document.getElementById('listActive')
-  const titleOrigens = document.getElementsByClassName('titleOrigens');
-
-  if (document.getElementById('listActive')) {
-    console.log(document.getElementById('listActive').textContent);
-  }
-
-
-
 }
-
-
-/*
-function clickListener(e){
-    var el = document.querySelector('h4')
-    e = el.textContent
-}
-*/
 
 
 // VER RESULTADOS
 const outputHtml = matches => {
 
-
-
  if (matches.length > 0) {
   const html = matches
    .map(
     match => `<ul class="card card-body mb-1 listOrigin">
-    <li><h4 id="listOrigin" class="titleOrigens" onclick="clickListener(this)">(${match.IATA}) ${match.CIDADE} </li>
+    <li><h4 id="listOrigin" class="titleOrigens" onclick="">${match.IATA} - ${match.CIDADE} </li>
    </ul>`
    )
    .join('');
 
   matchList.innerHTML = html;
 
-
-
  }
 
-
-
-
+}
+  outputHtml(matches);
 
 };
 
 
 
+document.getElementById("origensWrap").addEventListener("click",(e) => {
+  if (e.target && e.target.matches("li h4")) {
+    searchOrigens.value = e.target.innerText.substr(0,3)
+  }
+});
+
+document.getElementById("destinoWrap").addEventListener("click",(e) => {
+ if (e.target && e.target.matches("li h4")) {
+   searchDestino.value = e.target.innerText.substr(0,3)
+ }
+});
 
 
-window.addEventListener('DOMContentLoaded', pegarOrigens);
-search.addEventListener('input', () => searchStates(search.value));
+
+window.addEventListener('DOMContentLoaded', getlocals);
+searchOrigens.addEventListener('input', () => searchStates(searchOrigens.value));
+
+searchDestino.addEventListener('input', () => searchStates(searchDestino.value));
